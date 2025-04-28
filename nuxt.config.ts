8d8a62af78bfb3ab609ff3511467bfefd81f96c6
Captain K <Tooriskygh@gmail.com>
2025-04-28 11:24:30 +0000

    google analytics added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' }
   },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@pinia/nuxt", "nuxt-vuefire"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@pinia/nuxt", "nuxt-vuefire", "nuxt-gtag"],
 
   css: ["~/assets/css/tailwind.css"],
 
